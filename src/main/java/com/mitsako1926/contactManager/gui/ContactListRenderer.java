67c766ab8e3b027b8fc516f0ef34b1d60fac706b
mitsik <mitsako2006@gmail.com>
2026-04-19 18:37:19 +0300

    the ui is done (maybe some adjustments needed)

---
+++ src/main/java/com/mitsako1926/contactManager/gui/ContactListRenderer.java
@@ -0,0 +1,71 @@
+package com.mitsako1926.contactManager.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
+import javax.swing.BorderFactory;
+
+import com.mitsako1926.contactManager.model.Contact;
+
+public final class ContactListRenderer extends JPanel implements ListCellRenderer<Contact> {
+
+    private final JLabel iconLabel = new JLabel();
+    private final JLabel nameLabel = new JLabel();
+
+    
+    public ContactListRenderer() {
+        setLayout(new BorderLayout(10, 0));
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        add(iconLabel, BorderLayout.WEST);
+        add(nameLabel, BorderLayout.CENTER);
+    }
+    
+    
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Contact> list, Contact contact, int index, boolean isSelected, boolean cellHasFocus) {
+
+        nameLabel.setText(contact.toString());
+
+        ImageIcon icon = loadIcon(contact);
+        iconLabel.setIcon(icon);
+
+        if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            nameLabel.setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            nameLabel.setForeground(list.getForeground());
+        }
+
+        setOpaque(true);
+        
+        return this;
+    }
+
+    
+    
+    private ImageIcon loadIcon(Contact contact) {
+        ImageIcon icon;
+
+        if (contact.getImagePath() != null && !contact.getImagePath().isBlank()) {
+            icon = new ImageIcon(contact.getImagePath());
+        } else {
+            icon = new ImageIcon(getClass().getResource("/images/users/user.png"));
+        }
+
+        Image scaled = icon.getImage().getScaledInstance(28, 28, Image.SCALE_SMOOTH);
+        
+        return new ImageIcon(scaled);
+    }
+    
+    
+    
+}
--- src/main/java/com/mitsako1926/contactManager/gui/ContactManagerCenterPanel.java
+++ src/main/java/com/mitsako1926/contactManager/gui/ContactManagerCenterPanel.java
@@ -1,16 +1,111 @@
 package com.mitsako1926.contactManager.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import com.mitsako1926.contactManager.model.Contact;
+
 
 public final class ContactManagerCenterPanel extends JPanel{
 
+	
+	private final JPanel topPanel = new JPanel(new BorderLayout());
+	private final JPanel contactsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+	
+	private final JLabel labelContacts = new JLabel("    All Contacts");
+	private final JLabel labelSortBy = new JLabel("Sort By");
+	
+	private final DefaultListModel<Contact> model;
+    
+	private final JList<Contact> list;
+	
+	private final JComboBox<String> comboBox = new JComboBox<String>();
+	
 	ContactManagerCenterPanel(){
-		setBackground(Color.BLUE);
 		setPreferredSize(new Dimension(250,500));
-		setOpaque(true);
+		setLayout(new BorderLayout());
+		
+		
+		//TOP PANEL
+		topPanel.setPreferredSize(new Dimension(300,40));
+		
+		comboBox.setPreferredSize(new Dimension(100,25));
+		comboBox.addItem("First Name");
+		comboBox.addItem("Last Name");
+		comboBox.addItem("Favorites First");
+		
+		JPanel panelSortBy = new JPanel(new FlowLayout(1,5,8));
+		panelSortBy.add(labelSortBy);
+		panelSortBy.add(comboBox);
+		
+		topPanel.add(labelContacts,BorderLayout.WEST);
+		topPanel.add(panelSortBy,BorderLayout.EAST);
+		
+		
+		contactsPanel.setOpaque(true);
+		contactsPanel.setBackground(Color.PINK);
+		contactsPanel.setLayout(new BorderLayout());
+		contactsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		topPanel.setOpaque(true);
+		topPanel.setBackground(Color.WHITE);
+		
+		
+		//CONTACTS PANEL
+		
+		model = new DefaultListModel<Contact>();
+		
+		
+		//CUSTOMIZE LIST
+		for (int i = 1; i <= 100; i++) {
+		    Contact c = new Contact(
+		        "Name" + i,
+		        "Surname" + i,
+		        "690000000" + i,
+		        "user" + i + "@mail.com",
+		        "Company" + i,
+		        "Notes " + i,
+		        false,
+		        null
+		    );
+		    model.addElement(c);
+		    
+		}
+		list = new JList<Contact>(model);
+		list.setCellRenderer(new ContactListRenderer());
+		list.setFont(new Font("Arial", Font.PLAIN, 18));
+		list.setBackground(Color.GRAY);
+		list.setForeground(Color.WHITE);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setFont(new Font("Dialog",Font.PLAIN,17));
+		list.addMouseListener(new MouseAdapter() {
+		    @Override
+		    public void mouseClicked(MouseEvent e) {
+		        if (e.getClickCount() == 2) {
+		        	System.out.println(0);
+		        }
+		    }
+		});
+		
+		contactsPanel.add(new JScrollPane(list),BorderLayout.CENTER);
+		
+		//ADD EVERYTHING TO THE MAIN PANEL
+		add(topPanel,BorderLayout.NORTH);
+		add(contactsPanel,BorderLayout.CENTER);
 	}
 	
 	
