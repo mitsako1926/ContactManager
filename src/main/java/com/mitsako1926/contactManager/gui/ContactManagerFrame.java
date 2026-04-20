004aededc72ce9ea798c7806658eba94f887e094
mitsik <mitsako2006@gmail.com>
2026-04-21 02:02:59 +0300

    app basic functions are done(find bugs, expand functionality)

--- src/main/java/com/mitsako1926/contactManager/gui/ContactManagerFrame.java
+++ src/main/java/com/mitsako1926/contactManager/gui/ContactManagerFrame.java
@@ -2,6 +2,7 @@ package com.mitsako1926.contactManager.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
@@ -15,12 +16,14 @@ public final class ContactManagerFrame extends JFrame{
 	public ContactManagerFrame(){
 		
 		ImageIcon imageIcon = new ImageIcon(getClass().getResource("/images/icons/frameIcon.png"));
+		Image img = imageIcon.getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH);
+		
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setTitle("Contact Manager");
-		setIconImage(imageIcon.getImage());
+		setIconImage(img);
 		setLayout(new BorderLayout());
 		
 		ContactService service = new ContactService();
